ce7bb0c008239a16ce0188628cab55cf720e3fab
Xiaofeng Ma <i.xiaofeng.ma@outlook.com>
2022-06-28 16:57:56 +0300

    test script

--- script_test.m
+++ script_test.m
@@ -8,7 +8,7 @@ foot_imu.acc = foot(:,1:3); %转为m/s^2
 foot_imu.gyros = foot(:,4:6); %转为rad/s
 foot_imu.ts = 1/100;
 
-filename = 'C:\04 MyCodes\MyCodes\data\新主楼\BayesProj.xlsx';%'C:\04 MyCodes\MyCodes\data\DATA3.xlsx'
+filename = 'D:\04 MyCodes\MyCodes\data\新主楼\BayesProj.xlsx';%'C:\04 MyCodes\MyCodes\data\DATA3.xlsx'
 foot = xlsread(filename,1);
 
 filename = 'C:\04 MyCodes\MyCodes\data\20220518\mystream_5_18_19_11_4.csv';
@@ -34,7 +34,9 @@ foot_imu.gyros = (sSys2rfu('rfu')*foot(:,4:6)')' * glv.rad; %转为rad/s
 foot_imu.mag = (sSys2rfu('rfu')*foot(:,11:13)')'; %转为rad/s
 foot_imu.ts = 1/100;
 %%
-filename = 'D:\MXFcodes\MATLAB\MARG-IMU-based-in-door-positioning\Human movement\Harish stairs 1\VNYMR.csv';
+figure(1000);hold on;
+for trial=1:10
+filename = ['D:\MXFcodes\MATLAB\MARG-IMU-based-in-door-positioning\Human movement\Xiaofeng ',num2str(trial),'\VNYMR.csv'];
 foot = xlsread(filename,1);
 foot_imu.acc = (sSys2rfu('frd')*foot(:,7:9)')'; %转为m/s^2
 foot_imu.gyros = (sSys2rfu('frd')*foot(:,10:12)')'; %转为rad/s
@@ -46,7 +48,7 @@ order = 'zxy';
 
 foot_imu1 = foot_imu;
 %---------------初始粗对准-----------------
-still_time =50; 
+still_time =5; 
 [foot_q0,foot_att0] = alignbyAccMag(foot_imu1.acc(1:still_time,:),[],order);
 disp(['-----  初始对准角度（°/s）：',num2str((foot_att0*glv.deg)'),'  -----']);
 if abs(foot_att0(1))>0.1 || abs(foot_att0(2))>0.1 %0727
@@ -66,12 +68,14 @@ BayesEKF
 
 N = length(MM_e');
 accne = zeros(N,3);
-    for i = 3:N
+    for i = 5:N
          accne(i,:) = (qua2dcm(MM_e(:,i)','Cnb')*imu.acc(i,:)' - [0;0;glv.g0])';
     end
     
  pose = posCalculateWithGait(imu,accne');
-    
+ figure(1000)
+ plot(pose(:,1),pose(:,2),'r-')
+end 
 % [pose,~,~,~,~,arr_gait_time,stationaryStart,stationaryEnd,stationary,~]=positionCalculate(imu,accne','foot');
 % figure;plot(pose(:,1),pose(:,2))
 % [pose]=posCalculate(imu,MM_e');
@@ -121,6 +125,9 @@ plot(posgh(:,1),posgh(:,2),'-.m','Linewidth',1);
 plot(posc(:,1),posc(:,2),':b','Linewidth',1);
 
 legend({'Reference','EKF','UKF','GHKF','CKF'});
+box on
 xlabel('X (m)');
 ylabel('Y (m)')
  set(gca,'FontSize',10,'FontName','Times New Roman');
+ ref_pos = [0,0;0,1.2*8;-1.2*2,1.2*8;-1.2*2,0;0,0];
+ plot(ref_pos(:,1),ref_pos(:,2),'g*')
