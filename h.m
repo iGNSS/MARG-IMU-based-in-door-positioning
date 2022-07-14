6a663a9043ef9e215d7521115259eac390c26de9
Xiaofeng Ma <i.xiaofeng.ma@outlook.com>
2022-07-15 00:09:56 +0300

    measurement equation

--- h.m
+++ h.m
@@ -1,6 +1,6 @@
- function [z_pre,z_obs,H] = h(state_type,X,obs_type,z_obs,para)
- q0 = X(1); q1 = X(2); q2 = X(3); q3 = X(4);
-
+ function [z_pre,z_obs,H] = h(state_type,X,obs_type,obs,para)
+q0 = X(1); q1 = X(2); q2 = X(3); q3 = X(4);
+z_obs = obs;
 if strcmp(obs_type,'gn')
     % ### The output of accelerometers should be equal to the grivaty
     % ### expressed in the b frame.
@@ -23,6 +23,7 @@ if strcmp(obs_type,'gn')
                  2*g*q0, -2*g*q1, -2*g*q2, 2*g*q3, 0, 0, 0, 0, 0, 1];
                  
         otherwise
+            error('wrong');
     end
 elseif strcmp(obs_type,'hnx')
     % ### The output of magnetometers should be equal to 
